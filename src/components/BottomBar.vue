e3d5b365de673b5271ec2d61d1d8c9184d1dd63f
LarenDorr <larendorrx@gmail.com>
2018-07-18 23:56:52 +0800

    update components

---
+++ src/components/BottomBar.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="bottom-bar">
+  asd
+</div>
+</template>
+<script>
+export default {
+  name: 'BottomBar',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+<style lang="sass">
+
+</style>
